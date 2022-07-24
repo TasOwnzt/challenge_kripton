afe87b89fcfa551333f7110b20c552115adacbbf
tasownzt <tas-93@hotmail.com>
2022-07-24 01:06:40 -0300

    Adding cofing file

--- configs.php
+++ configs.php
@@ -2,4 +2,4 @@
 
 // define('BASEDIR', __DIR__);
 define('APPDIR', join('/', ['src', 'views']));
-define('ASSETSDIR', join('/', [APPDIR, 'assets']));
+define('ASSETSDIR', join('/', [APPDIR, 'assets'])); // Quando for definida a rota mudar pra $_REQUEST['HTTP_HOST']
